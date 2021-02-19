233ff1c481586f47b00b9082646a5f39211eec09
thiennhanng191 <ntnguyen@vassar.edu>
2021-02-19 14:30:13 -0500

    displays titles from tmdb with basic landscape and portrait mode

---
+++ app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
@@ -0,0 +1,84 @@
+package com.example.flixter.adapters;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.flixter.R;
+import com.example.flixter.models.Movie;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
+    Context context;
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    // Usually involves inflating a layout from XML and returning the holder
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("MovieAdapter", "onCreateViewHolder");
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(movieView);
+    }
+
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("MovieAdapter", "onBindViewHolder" + position);
+
+        // Get the movie at the specified position
+        Movie movie = movies.get(position);
+
+        // Bind the movie data into the ViewHolder
+        holder.bind(movie);
+    }
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvMovieTitle;
+        TextView tvMovieOverview;
+        ImageView ivMoviePoster;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvMovieTitle = itemView.findViewById(R.id.textViewMovieTitle);
+            tvMovieOverview = itemView.findViewById(R.id.textViewMovieOverview);
+            ivMoviePoster = itemView.findViewById(R.id.itemViewMoviePoster);
+        }
+
+        public void bind(Movie movie) {
+            tvMovieTitle.setText(movie.getTitle());
+            tvMovieOverview.setText(movie.getOverview());
+
+            String imageUrl;
+            // if phone is in landscape
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                imageUrl = movie.getBackdropPath();
+            } else {  // else in vertical
+                imageUrl = movie.getPosterPath();
+            }
+            //displaying image with Glide
+            Glide.with(context).load(imageUrl).into(ivMoviePoster);
+        }
+    }
+}
